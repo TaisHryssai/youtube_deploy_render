6643fee03a9ea527e74d1beaeb8536b22e0d7b02
TaisHryssai <thryssai@gmail.com>
2024-06-20 22:49:57 -0300

    Deploy com render

--- src/main/java/br/com/danieleleaoe/deploy/DeployRender.java
+++ src/main/java/br/com/danieleleaoe/deploy/DeployRender.java
@@ -2,8 +2,10 @@ package br.com.danieleleaoe.deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class DeployRender {
 
 	public static void main(String[] args) {
